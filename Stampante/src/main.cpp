7679c03c3296f1ce7cdc50bd908e911648ff35e0
unknown <carfoggia26@gmail.com>
2020-12-23 01:06:24 +0100

    Created main window

---
+++ Stampante/src/main.cpp
@@ -0,0 +1,80 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Carmine Foggia
+   ======================================================================== */
+#include "..\resource.h"
+#include <windows.h>
+#include <winuser.h>
+#include <synchapi.h>
+
+bool Quit = false;
+HWND Window;
+
+LRESULT CALLBACK MainWindowCallback
+(HWND   hWnd,
+ UINT   uMsg,
+ WPARAM wParam,
+ LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        case WM_CLOSE:
+        case WM_DESTROY:
+        case WM_QUIT:
+        {
+            Quit = true;
+            return 0;
+            break;
+        }
+        default:
+        {
+            return CallWindowProc(DefWindowProc, hWnd, uMsg, wParam, lParam);
+        }
+    }
+}
+
+int CALLBACK WinMain
+(HINSTANCE hInstance,
+ HINSTANCE hPrevInstance,
+ LPSTR     lpCmdLine,
+ int       nCmdShow)
+{
+    WNDCLASS WindowClass {};
+    WindowClass.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    WindowClass.lpfnWndProc   = MainWindowCallback;
+    WindowClass.cbClsExtra    = 0;
+    WindowClass.cbWndExtra    = 0;
+    WindowClass.hInstance     = hInstance;
+    WindowClass.hIcon         = LoadIcon(hInstance, MAKEINTRESOURCE(TOOLBAR_ICON));
+    WindowClass.hCursor       = LoadCursor(0, MAKEINTRESOURCE(IDC_ARROW));
+    WindowClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    //   WindowClass.lpszMenuName  = MAKEINTRESOURCE(IDR_MENU1);
+    WindowClass.lpszClassName = "PrinterWindowClass";
+    RegisterClass(&WindowClass);
+    Window = CreateWindow("PrinterWindowClass",
+                          "La stampante pazza sgravata",
+                          WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+    while(!Quit)
+    {
+        MSG Message;
+        while(PeekMessage(&Message, 0, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&Message);
+            DispatchMessage(&Message);
+        }
+        timeBeginPeriod(1);
+        Sleep(16);
+        timeEndPeriod(1);
+    }
+    return TRUE;
+}
