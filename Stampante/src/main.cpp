ffb1125768bb75a66fce5fe0d8a0129adbd87cf4
unknown <carfoggia26@gmail.com>
2020-12-25 00:41:34 +0100

    Added rudimentary GUI. Should be enough to get the printer's basic features working

--- Stampante/src/main.cpp
+++ Stampante/src/main.cpp
@@ -11,6 +11,60 @@
 
 bool Quit = false;
 HWND Window;
+HWND SendDialog;
+bool isSendOpen = false;
+char Image[256];
+
+LRESULT CALLBACK StatusCallback
+(HWND   hWnd,
+ UINT   uMsg,
+ WPARAM wParam,
+ LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        default:
+        {
+            return CallWindowProc(DefWindowProc, hWnd, uMsg, wParam, lParam);
+        }
+    }
+}
+
+INT_PTR SendDialogProc
+(HWND   hWnd,
+ UINT   uMsg,
+ WPARAM wParam,
+ LPARAM lParam)
+{
+    switch(uMsg)
+    {
+        case WM_CLOSE:
+        case WM_DESTROY:
+        case WM_QUIT:
+        {
+            DestroyWindow(hWnd);
+            isSendOpen = false;
+            SendDialog = NULL;
+            return TRUE;
+            break;
+        }
+        case WM_INITDIALOG:
+        {
+            SendDialog = hWnd;
+            return TRUE;
+            break;
+        }
+        case WM_DRAWITEM:
+        {
+            return CallWindowProc(DefWindowProc, hWnd, uMsg, wParam, lParam);
+            break;
+        }
+        default:
+        {
+            return FALSE;
+        }
+    }
+}
 
 LRESULT CALLBACK MainWindowCallback
 (HWND   hWnd,
@@ -28,6 +82,54 @@ LRESULT CALLBACK MainWindowCallback
             return 0;
             break;
         }
+        
+        case WM_COMMAND:
+        {
+            if(!(wParam & 0xFFFF0000))
+            {
+                switch(wParam & 0xFFFF)
+                {
+                    case ID_IMAGE:
+                    {
+                        OPENFILENAME OpenFile {};
+                        OpenFile.lStructSize = sizeof(OPENFILENAME);
+                        OpenFile.hwndOwner = hWnd;
+                        OpenFile.hInstance = GetModuleHandle(0);
+                        OpenFile.lpstrFilter = TEXT("PNG images\0*.png\0\0");
+                        OpenFile.lpstrFile = Image;
+                        OpenFile.nMaxFile = 255;
+                        OpenFile.Flags = OFN_FILEMUSTEXIST;
+                        OpenFile.lpstrDefExt = TEXT("png");
+                        GetOpenFileName(&OpenFile);
+                        return 0;
+                        break;
+                    }
+                    case ID_SEND:
+                    {
+                        if(!isSendOpen)
+                        {
+                            CreateDialog(GetModuleHandle(NULL),
+                                                      MAKEINTRESOURCE(IDD_SENDDIALOG),
+                                                      hWnd, SendDialogProc);
+                                ShowWindow(SendDialog, SW_SHOW);
+                                isSendOpen = true;
+                        }
+                        return 0;
+                        break;
+                    }
+                    default:
+                    {
+                        return CallWindowProc(DefWindowProc, hWnd, uMsg, wParam, lParam);
+                    }
+                }
+            }
+            else
+            {
+                return CallWindowProc(DefWindowProc, hWnd, uMsg, wParam, lParam);
+            }
+            break;
+        }
+        
         default:
         {
             return CallWindowProc(DefWindowProc, hWnd, uMsg, wParam, lParam);
@@ -53,6 +155,20 @@ int CALLBACK WinMain
     WindowClass.lpszMenuName  = MAKEINTRESOURCE(IDR_MAINMENU);
     WindowClass.lpszClassName = "PrinterWindowClass";
     RegisterClass(&WindowClass);
+
+    WNDCLASS StatusClass {};
+    StatusClass.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    StatusClass.lpfnWndProc   = StatusCallback;
+    StatusClass.cbClsExtra    = 0;
+    StatusClass.cbWndExtra    = 0;
+    StatusClass.hInstance     = hInstance;
+    StatusClass.hIcon         = NULL;
+    StatusClass.hCursor       = LoadCursor(0, MAKEINTRESOURCE(IDC_ARROW));
+    StatusClass.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+    StatusClass.lpszMenuName  = NULL;
+    StatusClass.lpszClassName = "StatusClass";
+    RegisterClass(&StatusClass);
+    
     Window = CreateWindow("PrinterWindowClass",
                           "La stampante pazza sgravata",
                           WS_OVERLAPPEDWINDOW | WS_VISIBLE,
